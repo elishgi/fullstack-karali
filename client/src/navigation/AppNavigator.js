875c7153a4bdf321ebb51015266c8083e804178d
elishgi <wwmn7701@gmail.com>
2025-11-03 14:46:00 +0200

    Add loading transition screen before entering home

--- client/src/navigation/AppNavigator.js
+++ client/src/navigation/AppNavigator.js
@@ -9,6 +9,7 @@ import LogsScreen from '../screens/LogsScreen';
 import AddDetailedLogScreen from '../screens/AddDetailedLogScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import LoginScreen from '../screens/LoginScreen';
+import LoadingScreen from '../screens/LoadingScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +19,7 @@ export default function AppNavigator() {
       <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Loading" component={LoadingScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="AddEvent" component={AddEventScreen} />
         <Stack.Screen name="Logs" component={LogsScreen} />
